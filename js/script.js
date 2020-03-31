b48ec1b76c50341958ecbf78cea8182d8a6c0d08
whoami <whoami@localhost.localdomain>
2020-03-31 08:53:46 -0400

    Reallx effect

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let rellax = new Rellax('.rellax');
+
+// (function($) {
+//     var $window = $(window),
+//         $html = $('html');
+
+//     function resize() {
+//         if ($window.width() < 514) {
+//             $("div").removeClass("horizontal");
+//         }
+
+//     }
+
+//     $window
+//         .resize(resize)
+//         .trigger('resize');
+// })(jQuery);
